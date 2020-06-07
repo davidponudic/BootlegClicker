5ab2b75b634e30ccd015996b45855629d1eae956
mrakanze <mrakanze@gmail.com>
2020-06-07 16:24:11 +0200

    še enkrat popravu  lvle

--- First Form App/Form1.cs
+++ First Form App/Form1.cs
@@ -328,63 +328,63 @@ namespace First_Form_App
                 {
                     case 1:
                         barLove.Value = 0;
-                        barLove.Maximum = 400;
+                        barLove.Maximum = 880;
                         level++;
                         MessageBox.Show("You leveled up!");
                         break;
 
                     case 2:
                         barLove.Value = 0;
-                        barLove.Maximum = 880;
+                        barLove.Maximum = 1440;
                         level++;
                         MessageBox.Show("You leveled up!");
                         break;
                     case 3:
                         barLove.Value = 0;
-                        barLove.Maximum = 1440;
+                        barLove.Maximum = 2080;
                         level++;
                         MessageBox.Show("You leveled up!");
                         break;
                     case 4:
 
                         barLove.Value = 0;
-                        barLove.Maximum = 2080;
+                        barLove.Maximum = 2800;
                         level++;
                         MessageBox.Show("You leveled up!");
                         break;
                     case 5:
                         barLove.Value = 0;
-                        barLove.Maximum = 2800;
+                        barLove.Maximum = 3600;
                         level++;
                         MessageBox.Show("You leveled up!");
                         break;
                     case 6:
                         barLove.Value = 0;
-                        barLove.Maximum = 3600;
+                        barLove.Maximum = 4480;
                         level++;
                         MessageBox.Show("You leveled up!");
                         break;
                     case 7:
                         barLove.Value = 0;
-                        barLove.Maximum = 4480;
+                        barLove.Maximum = 5440;
                         level++;
                         MessageBox.Show("You leveled up!");
                         break;
                     case 8:
                         barLove.Value = 0;
-                        barLove.Maximum = 5440;
+                        barLove.Maximum = 6480;
                         level++;
                         MessageBox.Show("You leveled up!");
                         break;
                     case 9:
                         barLove.Value = 0;
-                        barLove.Maximum = 6480;
+                        barLove.Maximum = 7600;
                         level++;
                         MessageBox.Show("You leveled up!");
                         break;
                     case 10:
                         barLove.Value = 0;
-                        barLove.Maximum = 7600;
+                        barLove.Maximum = 8800;
                         level++;
                         MessageBox.Show("You leveled up!");
                         break;
